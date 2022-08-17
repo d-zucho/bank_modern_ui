a936c5aaca6a0bd38cfef02efe0e32f5f68f1085
D_Zucho <danny.nico.dev@outlook.com>
2022-08-17 01:48:02 -0700

    start nav bar desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,44 @@
+import styles from './styles'
+import {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+} from './components/index.js'
+
 const app = () => (
   <div className='bg-primary w-full h-full overflow-hidden'>
-    <h1 className='text-lg text-cyan-700'>tester</h1>
+    {/* **NAVBAR SECTION** */}
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+    {/* **HERO SECTION** */}
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
   </div>
 )
 
